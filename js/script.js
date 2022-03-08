b1b1afe827e10d8d1b7634749b0742dfb391fc5d
AntonioStr <antonio.straziota10@gmail.com>
2022-03-08 05:37:08 +0100

    aggiunta script 2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 // // +++ JSnack 1 +++
 
-let somma = 0;
- for
-for (let i = 0; i < 5; i++) {
-    somma = somma + parseInt(prompt("Inserisci un numero da sommare:"));
-}
- while
-let i = 0;
-while (i < 5) {
-    somma = somma + parseInt(prompt("Inserisci un numero da sommare:"));
-    i++;
-}
-console.log("La somma è: " + somma);
+// let somma = 0;
+// // for
+// for (let i = 0; i < 5; i++) {
+//     somma = somma + parseInt(prompt("Inserisci un numero da sommare:"));
+// }
+// // while
+// let i = 0;
+// while (i < 5) {
+//     somma = somma + parseInt(prompt("Inserisci un numero da sommare:"));
+//     i++;
+// }
+// console.log("La somma è: " + somma);
+
+
+// // +++ JSnack 2 +++
+
+// const word1 = prompt("Inserisci la prima parola: ");
+// const word2 = prompt("Inserisci la seconda parola: ");
+// if (word1.length > word2.length) {
+//     console.log(word1);
+// } else if (word1.length < word2.length) {
+//     console.log(word2);
+// } else {
+//     console.log(word1 + " " + word2);
+// }
 
